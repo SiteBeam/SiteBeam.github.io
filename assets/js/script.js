9567d1ce946857191901c85606e4396effc72c1b
Jul879n <arayacontrerasjulian@gmail.com>
2023-07-24 23:20:37 -0400

    casi
    me falta actualiza iconos e informacion de contacto, ademas del formulario y animaciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-    $("#primera #contenido").load("info/texto.markdown", function() {
+$(document).ready(function () {
+    $("#primera #contenido").load("info/texto.markdown", function () {
         var converter = new showdown.Converter();
         var markdownText = $('#contenido').text();
         var html = converter.makeHtml(markdownText);
@@ -7,7 +7,7 @@ $(document).ready(function() {
         $(this).find("img").addClass("card-img");
         $(this).find("a").addClass("btn btn-tema rounded-pill mt-2");
     });
-    $("#navEnlace").load("info/nav.markdown", function() {
+    $("#navEnlace").load("info/nav.markdown", function () {
         var converter = new showdown.Converter();
         var markdownText = $('#navEnlace').text();
         var html = converter.makeHtml(markdownText);
@@ -19,7 +19,7 @@ $(document).ready(function() {
         $(this).find("li").eq(1).append($("#navEnlace").find("a").eq(1));
         $("#navEnlace").prepend($(".nav-item"));
     });
-    $("#sitioUno").load("info/sitios.markdown", function() {
+    $("#sitioUno").load("info/sitios.markdown", function () {
         var converter = new showdown.Converter();
         var markdownText = $("#sitioUno").text();
         var html = converter.makeHtml(markdownText);
@@ -27,11 +27,57 @@ $(document).ready(function() {
         $(this).find("img").addClass("card-img");
         $("#sitioDos").prepend($("#sitioUno").find("h2").eq(1));
         $("#sitioDos").append($("#sitioUno").find("a").eq(1));
-        $("p").each(function() {
+        $("p").each(function () {
             if ($(this).length == 0) {
-              $(this).addClass("empty-p");
+                $(this).addClass("empty-p");
             }
-          });
-          $(".empty-p").remove();
+        });
+        $(".empty-p").remove();
     });
+    //carrusel sitios
+    $("#carrusel").slick({
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+        pauseOnHover: true,
+        infinite: true,
+        edgeFriction: 1,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    autoplay: true,
+                    autoplaySpeed: 4000,
+                },
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 4000,
+                },
+            },
+            {
+                breakpoint: 375,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 4000,
+                },
+            },
+        ],
+    });
+
+    var fechaActual = new Date();
+    // Obtener año actual
+    var anioActual = fechaActual.getFullYear();
+    $("#anio").text(anioActual);
 });
