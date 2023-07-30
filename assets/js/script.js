1a4b41218af02af7c76392d4e1c46c1802be0dc9
Jul879n <arayacontrerasjulian@gmail.com>
2023-07-29 20:09:06 -0400

    tipo sitios
    se agregaron tipos de sitios y sus valores

--- assets/js/script.js
+++ assets/js/script.js
@@ -84,11 +84,11 @@ $(document).ready(function () {
     $("#contacto #asunto").change(function () {
         var asunto = $("#contacto #asunto").val();
         if (asunto == "Otro") {
-          $("#contacto #otro").removeClass("transparente");
+            $("#contacto #otro").removeClass("transparente");
         } else {
-          $("#contacto #otro").addClass("transparente");
+            $("#contacto #otro").addClass("transparente");
         }
-      });
+    });
     $("#basico").click(function () {
         $(".plan").removeClass("active")
         $(this).addClass("active")
@@ -103,5 +103,50 @@ $(document).ready(function () {
         $(".plan").removeClass("active")
         $(this).addClass("active")
         $("#contacto #asunto").val("Plan premium");
+    });
+
+    $("#diseno-1").load("info/diseno-1.md", function () {
+        var converter = new showdown.Converter();
+        var markdownText = $("#diseno-1").html();
+        var html = converter.makeHtml(markdownText);
+        $('#diseno-1').html(html);
+        var contenido = $('#diseno-1').html();
+        $("#diseno-1").empty();
+        $("#diseno-1").addClass("container pt-3 pb-3");
+        $("#diseno-1").append('<div class="card bg-tema-secundario border-0"></div>');
+        $("#diseno-1 .card").append('<div class="card-body text-tema-secundario">' + contenido + '</div>');
+        $("#diseno-1 .card-body").append('<div id="tiposPage" class="row"></div>');
+        //one page
+        $("#diseno-1 #tiposPage").append('<div id="onePage" class="col-12 col-sm-4"></div>');
+        $("#diseno-1 #onePage").append('<div class="card border-0 bg-tema"></div>');
+        $("#diseno-1 #onePage .card").append('<div class="card-body text-tema"></div>');
+        var titulo1 = $("#diseno-1 h3").eq(0).html();
+        var precio1 = $("#diseno-1 h3").eq(1).html();
+        var lista1 = $("#diseno-1 ul").eq(0).html();
+        var titulo2 = $("#diseno-1 h3").eq(2).html();
+        var precio2 = $("#diseno-1 h3").eq(3).html();
+        var lista2 = $("#diseno-1 ul").eq(1).html();
+        var titulo3 = $("#diseno-1 h3").eq(4).html();
+        var precio3 = $("#diseno-1 h3").eq(5).html();
+        var lista3 = $("#diseno-1 ul").eq(2).html();
+        $("#diseno-1 h3").remove();
+        $("#diseno-1 ul").remove();
+        $("#diseno-1 #onePage .card-body").append('<div class="row"><div class="col-6"><span class="fs-4">'+titulo1+'</span></div><div class="col-6"><span class="fs-4 fw-bold">'+precio1+'</span></div> </div>');
+        $("#diseno-1 #onePage .card-body").append('<div class="linea-secundaria rounded-pill mt-2 mb-2"></div>');
+        $("#diseno-1 #onePage .card-body").append(lista1);
+        //landing page
+        $("#diseno-1 #tiposPage").append('<div id="landingPage" class="col-12 col-sm-4"></div>');
+        $("#diseno-1 #landingPage").append('<div class="card border-0 bg-tema"></div>');
+        $("#diseno-1 #landingPage .card").append('<div class="card-body text-tema"></div>');
+        $("#diseno-1 #landingPage .card-body").append('<div class="row"><div class="col-6"><span class="fs-4">'+titulo2+'</span></div><div class="col-6"><span class="fs-4 fw-bold">'+precio2+'</span></div> </div>');
+        $("#diseno-1 #landingPage .card-body").append('<div class="linea-secundaria rounded-pill mt-2 mb-2"></div>');
+        $("#diseno-1 #landingPage .card-body").append(lista2);
+        //empresa page
+        $("#diseno-1 #tiposPage").append('<div id="empresaPage" class="col-12 col-sm-4"></div>');
+        $("#diseno-1 #empresaPage").append('<div class="card border-0 bg-tema"></div>');
+        $("#diseno-1 #empresaPage .card").append('<div class="card-body text-tema"></div>');
+        $("#diseno-1 #empresaPage .card-body").append('<div class="row"><div class="col-6"><span class="fs-4">'+titulo3+'</span></div><div class="col-6"><span class="fs-4 fw-bold">'+precio3+'</span></div> </div>');
+        $("#diseno-1 #empresaPage .card-body").append('<div class="linea-secundaria rounded-pill mt-2 mb-2"></div>');
+        $("#diseno-1 #empresaPage .card-body").append(lista3);
     });
 });
---
+++ info/diseno-1.md
@@ -0,0 +1,32 @@
+## Tipos de sitios
+### One Page
+### $50.000
+- Una sola  pagina con hasta 5 secciones(inicio, quienes somos, servicios, contacto, etc.)
+- Formulario contacto
+- Dominio y hosting por un año
+- Almacenamiento 500mb
+- Diseño celular y escritorio
+- Autoadministrable
+- Certificado SSL
+- Asesoría por 365 días
+
+### Landing Page
+### $50.000
+- Hasta 5 secciones(inicio, quienes somos, servicios, contacto, etc.)
+- Formulario contacto
+- Dominio y hosting por un año
+- Almacenamiento 500mb
+- Diseño celular y escritorio
+- Autoadministrable
+- Certificado SSL
+- Asesoría por 365 días
+### Sitio Empresa
+### $130.000
+- Hasta 5 paginas (inicio, quienes somos, servicios, contacto, etc.)
+- Formulario contacto
+- Dominio y hosting por un año
+- Almacenamiento 500mb
+- Diseño celular y escritorio
+- Autoadministrable
+- Certificado SSL
+- Asesoría por 365 días
